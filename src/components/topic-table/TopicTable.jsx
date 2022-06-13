60a0e35a60cd5ea28f096a9eb34440a0deb4f90c
Zanfirache Andrada <zanfiracheandrada@gmail.com>
2022-06-13 20:07:38 +0300

    table

--- src/components/topic-table/TopicTable.jsx
+++ src/components/topic-table/TopicTable.jsx
@@ -1,1 +1,26 @@
+import React from "react";
 
+export const TopicTable = (props) => {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Resources</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.topic.map((key) => {
+            return (
+              <tr>
+                <td>{key.props.topic.name}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
