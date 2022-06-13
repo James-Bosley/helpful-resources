5686b26fc3cd4c8a5e2438ab1e804efc1482ce64
Zanfirache Andrada <zanfiracheandrada@gmail.com>
2022-06-13 20:39:29 +0300

    edits on table

--- src/components/topic-table/TopicTable.jsx
+++ src/components/topic-table/TopicTable.jsx
@@ -5,19 +5,29 @@ const TopicTable = (props) => {
     <div>
       <table>
         <thead>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Resources</th>
+          <tr className="table__header-row">
+            <th className="table__header">Name</th>
+            <th className="table__header">Description</th>
+            <th className="table__header">Resources</th>
           </tr>
         </thead>
         <tbody>
           {props.topics.map((key) => {
             return (
-              <tr>
-                <td>{key.name}</td>
-                <td>{key.description}</td>
-                <td>{key.resources}</td>
+              <tr className="table__row-body">
+                <td className="table__details"> {key.name}</td>
+                <td className="table__details">{key.description}</td>
+                <td className="table__details">
+                  {key.resources.map(() => {
+                    return (
+                      <ul>
+                        <li>
+                          <a href={key.resources}>{key.resources}</a>
+                        </li>
+                      </ul>
+                    );
+                  })}
+                </td>
               </tr>
             );
           })}
