1a081ed1f658f546b9c4124b6cf764b286420c63
James Bosley <james_bosley@hotmail.co.uk>
2022-06-13 14:13:53 +0100

    App state added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import { Routes, Route } from "react-router-dom";
+import { useState, useEffect, useCallback } from "react";
+import { getArticleList } from "./util/api.mjs";
 import Header from "./components/header/Header";
 import SideNav from "./components/side-nav/SideNav";
 import Home from "./components/home/Home";
 import Article from "./components/article/Article";
 import "./app.scss";
 
-function App() {
+const App = () => {
+  const [subjectList, setSubjectList] = useState([]);
+
+  const getList = useCallback(async () => {
+    const response = await getArticleList();
+    setSubjectList(response);
+  }, []);
+
+  useEffect(() => {
+    getList();
+  }, [getList]);
+
   return (
     <>
       <Header />
       <main>
-        <SideNav />
+        <SideNav list={subjectList} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/:article" element={<Article />} />
@@ -18,6 +31,6 @@ function App() {
       </main>
     </>
   );
-}
+};
 
 export default App;
---
+++ src/util/api.mjs
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:";
+
+export const getArticleList = async () => {
+  const { data } = await axios.get(`${BASE_URL}/content`);
+  return data;
+};
+
+export const getSingleArticle = async name => {
+  const { data } = await axios.get(`${BASE_URL}/content/${name}`);
+  return data;
+};
