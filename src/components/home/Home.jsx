5cb21dfffde64b8bb22a8963d6522e3413fc9a91
James Bosley <james_bosley@hotmail.co.uk>
2022-06-13 17:37:40 +0100

    Article and Homepage added.

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,24 @@
 import "./home.scss";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="homepage">
+      <h2 className="homepage__title">Welcome to Helpful Resources</h2>
+      <p className="homepage__info">
+        Congratualtions on making it this far on your web dev journey, we're sure it wasnt easy!
+      </p>
+      <p className="homepage__info">
+        Here we have put together a carefully curated selection of resources to help you understand
+        the topics you have covered in your lectures and labs. Some of the resources you will find
+        here are intended as deeper dives, so don't worry if you don't understand all of them at
+        this point - but come back in a few weeks and you might find you are already an expert.
+        Also, this means Joe doesn't have to worry about sending anthing out after the lectures
+        anymore. Win - win.
+      </p>
+      <p className="homepage__info">
+        To get started, click a topic on the left, or search for a topic.
+      </p>
+    </section>
+  );
 };
 export default Home;
