2c5bb5d26686bcd04389f8d601af1f3f34506107
James Bosley <james_bosley@hotmail.co.uk>
2022-06-13 14:58:00 +0100

    Header created.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,16 @@
+import logo from "../../assets/icons/logo.jpg";
 import "./header.scss";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="header__container">
+        <div className="header__icon-container">
+          <img src={logo} alt="Logo" className="header__icon" />
+        </div>
+        <p className="header__title">Synapse - Helpful Resources</p>
+      </div>
+    </div>
+  );
 };
 export default Header;
