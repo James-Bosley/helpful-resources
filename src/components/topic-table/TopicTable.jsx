bd011e556ffd5e2bce0d2356fc3b10bbf60d0f4e
James Bosley <james_bosley@hotmail.co.uk>
2022-06-13 20:57:01 +0100

    Table styles

--- src/components/topic-table/TopicTable.jsx
+++ src/components/topic-table/TopicTable.jsx
@@ -1,36 +1,25 @@
 import React from "react";
 import "./TopicTable.scss";
 
-const TopicTable = (props) => {
+const TopicTable = props => {
   return (
     <div>
       <table className="table">
-        {/* <thead className="table__header-wrapper"> */}
-        <div className="table__wrapper">
+        <thead className="table__header-wrapper">
           <tr className="table__header-row">
             <th className="table__header table__header-1">Name</th>
-          </tr>
-          <tr className="table__header-row">
             <th className="table__header table__header-2">Description</th>
-          </tr>
-          <tr className="table__header-row">
             <th className="table__header table__header-3">Resources</th>
           </tr>
-        </div>
-        {/* </thead> */}
+        </thead>
         <tbody>
-          {props.topics.map((key) => {
+          {props.topics.map(key => {
             return (
               <tr className="table__row-body">
-                <td className="table__details table__details--1 ">
-                  {" "}
-                  {key.name}
-                </td>
-                <td className="table__details table__details--2">
-                  {key.description}
-                </td>
+                <td className="table__details table__details--1 "> {key.name}</td>
+                <td className="table__details table__details--2">{key.description}</td>
                 <td className="table__details table__details--3">
-                  {key.resources.map((link) => {
+                  {key.resources.map(link => {
                     return (
                       <ul>
                         <li className="table__list">
